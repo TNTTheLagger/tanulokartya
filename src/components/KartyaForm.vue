c7ec599ce93e9a5a64544eaab41c824293eca2b0
Hazel <37238787+TNTTheLagger@users.noreply.github.com>
2024-11-19 13:56:34 +0100

    Finnished edit feature THE PROJECT IS NOW DONE

--- src/components/KartyaForm.vue
+++ src/components/KartyaForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <h5>Új kártya hozzáadása</h5>
+      <h5>{{ kartyaToEdit ? "Kártya szerkesztése" : "Új kártya hozzáadása" }}</h5>
       <div class="mb-2">
         <label>Kérdés</label>
         <input v-model="kerdes" type="text" class="form-control" />
@@ -18,13 +18,28 @@
           <option value="Nehéz">Nehéz</option>
         </select>
       </div>
-      <button @click="addKartya" class="btn btn-primary">Hozzáadás</button>
+      <button
+        @click="handleSubmit"
+        class="btn btn-primary"
+      >
+        {{ kartyaToEdit ? "Mentés" : "Hozzáadás" }}
+      </button>
+      <button
+        v-if="kartyaToEdit"
+        @click="clearEdit"
+        class="btn btn-secondary ms-2"
+      >
+        Mégse
+      </button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    kartyaToEdit: Object, // The card currently being edited
+  },
   data() {
     return {
       kerdes: "",
@@ -32,18 +47,50 @@ export default {
       nehézség: "Könnyű",
     };
   },
+  watch: {
+    kartyaToEdit: {
+      immediate: true,
+      handler(newVal) {
+        if (newVal) {
+          this.kerdes = newVal.kerdes;
+          this.valasz = newVal.valasz;
+          this.nehézség = newVal.nehézség;
+        } else {
+          this.resetForm();
+        }
+      },
+    },
+  },
   methods: {
-    addKartya() {
+    handleSubmit() {
       if (this.kerdes && this.valasz) {
-        this.$emit("add-kartya", {
-          kerdes: this.kerdes,
-          valasz: this.valasz,
-          nehézség: this.nehézség,
-        });
-        this.kerdes = "";
-        this.valasz = "";
-        this.nehézség = "Könnyű";
+        if (this.kartyaToEdit) {
+          // Emit update-kartya event if editing
+          this.$emit("update-kartya", {
+            id: this.kartyaToEdit.id,
+            kerdes: this.kerdes,
+            valasz: this.valasz,
+            nehézség: this.nehézség,
+          });
+        } else {
+          // Emit add-kartya event if adding
+          this.$emit("add-kartya", {
+            kerdes: this.kerdes,
+            valasz: this.valasz,
+            nehézség: this.nehézség,
+          });
+        }
+        this.resetForm();
       }
+    },
+    clearEdit() {
+      this.$emit("clear-edit");
+      this.resetForm();
+    },
+    resetForm() {
+      this.kerdes = "";
+      this.valasz = "";
+      this.nehézség = "Könnyű";
     },
   },
 };
